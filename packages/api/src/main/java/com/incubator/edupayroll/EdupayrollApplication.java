8ff9d697f05e282e07eb9099a2af4de3dc1516d6
hayrullahalper <alperhayrullah@gmail.com>
2024-02-23 18:54:19 +0300

    feat: spring initialization

---
+++ packages/api/src/main/java/com/incubator/edupayroll/EdupayrollApplication.java
@@ -0,0 +1,13 @@
+package com.incubator.edupayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdupayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdupayrollApplication.class, args);
+	}
+
+}
