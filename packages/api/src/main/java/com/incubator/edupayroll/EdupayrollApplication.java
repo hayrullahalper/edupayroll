bae0714cc341c3e83d4006a2fb53c8121908a6f9
halis <isaycel2001@gmail.com>
2024-02-25 18:17:08 +0300

    feat(server): add start print statement

--- packages/api/src/main/java/com/incubator/edupayroll/EdupayrollApplication.java
+++ packages/api/src/main/java/com/incubator/edupayroll/EdupayrollApplication.java
@@ -1,13 +1,28 @@
 package com.incubator.edupayroll;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class EdupayrollApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EdupayrollApplication.class, args);
-	}
+    @Autowired
+    private ServerProperties properties;
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdupayrollApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onReady() {
+        String host = properties.getAddress().getHostAddress();
+        String port = properties.getPort().toString();
+
+        System.out.printf("\nedupayroll application started at \n\t+ http://%s:%s \uD83D\uDE80\uD83D\uDE80\n", host, port);
+    }
 
 }
