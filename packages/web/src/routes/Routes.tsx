ca8d4be5e7d00ba92c7136f41f6692c10224cf47
halis <isaycel2001@gmail.com>
2024-03-04 17:25:03 +0300

    feat(web): create paths and routes file

--- packages/web/src/routes/Routes.tsx
+++ packages/web/src/routes/Routes.tsx
@@ -1,12 +1,52 @@
-import { Route, Routes as BaseRoutes } from 'react-router-dom';
+import Div100vh from 'react-div-100vh';
+import { Navigate, Route, Routes as BaseRoutes } from 'react-router-dom';
 
-import Panel from './Panel';
+import AppLayout from '../layouts/AppLayout';
+import AuthLayout from '../layouts/AuthLayout';
+
 import paths from './paths';
+import Login from './Login';
+import Logout from './Logout';
+import Documents from './Documents';
+import RequireAuth from './RequireAuth';
+import RequireAnonymous from './RequireAnonymous';
 
 export default function Routes() {
 	return (
-		<BaseRoutes>
-			<Route path={paths.panel} element={<Panel />} />
-		</BaseRoutes>
+		<Div100vh>
+			<BaseRoutes>
+				<Route element={<RequireAuth />}>
+					<Route path="/" element={<Navigate replace to={paths.documents} />} />
+
+					<Route element={<RequireAnonymous />}>
+						<Route element={<AuthLayout />}>
+							<Route path={paths.login} element={<Login />} />
+							<Route path={paths.register} element="register" />
+						</Route>
+
+						<Route path={paths.forgotPassword} element="forgotPassword" />
+					</Route>
+
+					<Route element={<RequireAuth />}>
+						<Route path={paths.logout} element={<Logout />} />
+
+						<Route element={<AppLayout />}>
+							<Route path={paths.document} element="document" />
+							<Route path={paths.documents} element={<Documents />} />
+
+							<Route path={paths.teacher} element="teacher" />
+							<Route path={paths.teachers} element="teachers" />
+
+							<Route path={paths.exports} element="exports" />
+
+							<Route path={paths.school} element="school" />
+							<Route path={paths.profile} element="profile" />
+							<Route path={paths.settings} element="settings" />
+							<Route path={paths.changePassword} element="changePassword" />
+						</Route>
+					</Route>
+				</Route>
+			</BaseRoutes>
+		</Div100vh>
 	);
 }
--- packages/web/src/routes/paths.ts
+++ packages/web/src/routes/paths.ts
@@ -1,3 +1,21 @@
 export default {
-	panel: '/',
+	document: '/documents/:documentId',
+	documents: '/documents',
+
+	teacher: '/teachers/:teacherId',
+	teachers: '/teachers',
+
+	exports: '/exports',
+
+	login: '/login',
+	logout: '/logout',
+	register: '/register',
+	registerComplete: '/register/complete',
+	resetPassword: '/reset-password',
+	forgotPassword: '/forgot-password',
+
+	school: '/settings/school',
+	profile: '/settings/profile',
+	settings: '/settings',
+	changePassword: '/settings/change-password',
 } as const;
