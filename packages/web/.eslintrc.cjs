747900a83291c0790c6847b2bd8c5328dc38838e
halis <isaycel2001@gmail.com>
2024-03-04 17:23:10 +0300

    chore(web): some config and package installation

--- packages/web/.eslintrc.cjs
+++ packages/web/.eslintrc.cjs
@@ -1,57 +1,57 @@
-module.exports ={
-  root: true,
-  env: {
-    es2020: true,
-    browser: true,
-  },
-  extends: [
-    "airbnb",
-    "prettier",
-    "airbnb/hooks",
-    "airbnb-typescript",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:import/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:react-hooks/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: ["./tsconfig.json"]
-  },
-  overrides: [
-    {
-      files: ["*.d.ts"],
-      rules: {
-        "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-      },
-    },
-  ],
-  plugins: [
-    "react",
-    "prettier",
-    "react-refresh"
-  ],
-  rules: {
-    "no-console": "error",
-    "consistent-return": "off",
-    "prettier/prettier": "error",
-    "no-restricted-exports": "off",
-    "react/react-in-jsx-scope": "off",
-    "import/prefer-default-export": "off",
-    "react/jsx-props-no-spreading": "off",
-    "@typescript-eslint/indent": ["error", "tab"],
-    "react-refresh/only-export-components": [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    "import/order": ["error", {
-      "groups": ["builtin", "external", "internal", "parent", "sibling", "index"],
-      "newlines-between": "always",
-    }]
-  },
+module.exports = {
+	root: true,
+	env: {
+		es2020: true,
+		browser: true
+	},
+	extends: [
+		'airbnb',
+		'prettier',
+		'airbnb/hooks',
+		'airbnb-typescript',
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:import/recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:react-hooks/recommended'
+	],
+	parser: '@typescript-eslint/parser',
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: ['./tsconfig.json']
+	},
+	overrides: [
+		{
+			files: ['*.d.ts'],
+			rules: {
+				'no-unused-vars': 'off',
+				'@typescript-eslint/no-unused-vars': 'off'
+			}
+		}
+	],
+	plugins: [
+		'react',
+		'prettier',
+		'react-refresh'
+	],
+	rules: {
+		'no-console': 'error',
+		'consistent-return': 'off',
+		'prettier/prettier': 'error',
+		'no-restricted-exports': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'import/prefer-default-export': 'off',
+		'react/jsx-props-no-spreading': 'off',
+		'@typescript-eslint/indent': ['error', 'tab'],
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true }
+		],
+		'import/order': ['error', {
+			'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+			'newlines-between': 'always'
+		}]
+	}
 };
