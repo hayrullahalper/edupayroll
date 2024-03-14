d33d652f2dde34991c6508bef87c8e1e93d00a4b
halis <isaycel2001@gmail.com>
2024-03-15 01:10:04 +0300

    feat(server): update teacher controller

--- packages/api/src/main/java/com/incubator/edupayroll/controller/TeacherController.java
+++ packages/api/src/main/java/com/incubator/edupayroll/controller/TeacherController.java
@@ -1,33 +1,50 @@
 package com.incubator.edupayroll.controller;
 
-import com.incubator.edupayroll.repository.TeacherRepository;
+import com.incubator.edupayroll.dto.teacher.TeacherCreationDTO;
+import com.incubator.edupayroll.dto.teacher.TeacherDTO;
+import com.incubator.edupayroll.service.teacher.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/teachers")
 public class TeacherController {
 
-    private final TeacherRepository teacherRepository;
+    private final TeacherService teacherService;
 
     @Autowired
-    public TeacherController(TeacherRepository teacherDAO) {
-        this.teacherRepository = teacherDAO;
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
     }
 
-    @GetMapping("/")
-    public String getTeachers() {
-        return teacherRepository.findAll().toString();
+    @PostMapping({"", "/"})
+    public ResponseEntity<TeacherDTO> createTeacher(
+            @RequestBody TeacherCreationDTO teacherCreationDTO
+    ) {
+        var teacher = teacherService.create(teacherCreationDTO);
+
+        return ResponseEntity
+                .ok()
+                .body(teacher);
     }
 
-    @GetMapping("/{id}")
-    public String getTeachers(@PathVariable String id) {
-        return teacherRepository.findById(UUID.fromString(id)).toString();
+    @GetMapping({"", "/"})
+    public ResponseEntity<List<TeacherDTO>> getTeachers(
+            @RequestParam(value = "limit", required = false) Integer limit,
+            @RequestParam("offset") int offset
+    ) {
+        var teachers = teacherService.getAll(
+                Optional.ofNullable(limit).orElse(20),
+                offset
+        );
+
+        return ResponseEntity
+                .ok()
+                .body(teachers);
     }
 
 }
