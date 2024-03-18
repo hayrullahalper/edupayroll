4e0b54108e3db7ceea2bb72776b2e949a57d0004
hayrullahalper <alperhayrullah@gmail.com>
2024-03-18 17:36:09 +0300

    feat(server): add DocumentController

--- packages/api/src/main/java/com/incubator/edupayroll/controller/document/DocumentController.java
+++ packages/api/src/main/java/com/incubator/edupayroll/controller/document/DocumentController.java
@@ -1,9 +1,34 @@
 package com.incubator.edupayroll.controller.document;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.incubator.edupayroll.dto.document.DocumentDTO;
+import com.incubator.edupayroll.dto.document.DocumentUpdateDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/document")
 public class DocumentController {
+
+    @PostMapping({"", "/"})
+    public ResponseEntity<DocumentDTO> createDocument(@RequestBody DocumentDTO documentDTO) {
+
+        return ResponseEntity
+                .ok()
+                .body(documentDTO);
+    }
+
+    @PutMapping({"", "/"})
+    public ResponseEntity<DocumentUpdateDTO> updateDocument(@RequestBody DocumentUpdateDTO documentUpdateDTO) {
+        return ResponseEntity
+                .ok()
+                .body(documentUpdateDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteDocument(@PathVariable("id") Long id) {
+        return ResponseEntity
+                .ok()
+                .body("Deleted: " + id);
+    }
+
 }
