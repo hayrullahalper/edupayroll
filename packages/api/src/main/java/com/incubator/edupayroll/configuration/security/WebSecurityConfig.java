2d973abdf38f19cf3b9c10b876387ece3e4e10cc
halis <isaycel2001@gmail.com>
2024-06-07 20:17:58 +0300

    feat(server): add open api 3 feature

--- packages/api/src/main/java/com/incubator/edupayroll/configuration/security/WebSecurityConfig.java
+++ packages/api/src/main/java/com/incubator/edupayroll/configuration/security/WebSecurityConfig.java
@@ -65,6 +65,8 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth
                                 .requestMatchers("/auth/**").permitAll()
+                                .requestMatchers("/swagger-ui/**").permitAll()
+                                .requestMatchers("/v3/api-docs/**").permitAll()
                                 .anyRequest().authenticated()
                 );
 
