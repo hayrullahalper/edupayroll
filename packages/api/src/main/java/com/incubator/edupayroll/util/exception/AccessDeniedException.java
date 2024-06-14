ac268fe9782aac03032d90a649c6e42ea7a0340d
halis <isaycel2001@gmail.com>
2024-06-14 22:08:23 +0300

    feat(server): create access denied exception class

---
+++ packages/api/src/main/java/com/incubator/edupayroll/util/exception/AccessDeniedException.java
@@ -0,0 +1,13 @@
+package com.incubator.edupayroll.util.exception;
+
+import com.incubator.edupayroll.entity.user.UserEntity;
+
+public class AccessDeniedException extends RuntimeException {
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+
+    public static AccessDeniedException byUser(UserEntity user) {
+        return new AccessDeniedException("Access denied for user: " + user.toString());
+    }
+}
