7076ba82dfa96f1fcc8ee14f769448997aecfac1
halis <isaycel2001@gmail.com>
2024-06-30 14:07:52 +0300

    feat(server): create security config for test profile

---
+++ packages/api/src/main/java/com/incubator/edupayroll/configuration/security/SecurityTestConfig.java
@@ -0,0 +1,25 @@
+package com.incubator.edupayroll.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Profile("test")
+@EnableMethodSecurity
+public class SecurityTestConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
