1a023d45eb392724e9410b7dbb5313bd66843e5f
hayrullahalper <alperhayrullah@gmail.com>
2024-07-10 10:26:50 +0300

    feat(server): add forgot password endpoint

--- packages/api/src/main/java/com/incubator/edupayroll/controller/auth/AuthController.java
+++ packages/api/src/main/java/com/incubator/edupayroll/controller/auth/AuthController.java
@@ -3,6 +3,7 @@ package com.incubator.edupayroll.controller.auth;
 import com.incubator.edupayroll.dto.auth.*;
 import com.incubator.edupayroll.service.auth.AuthService;
 import com.incubator.edupayroll.service.token.TokenService;
+import com.incubator.edupayroll.service.user.UserService;
 import com.incubator.edupayroll.util.response.Response;
 import com.incubator.edupayroll.util.validation.Validation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +22,13 @@ public class AuthController {
 
     private final AuthService authService;
     private final TokenService tokenService;
+    private final UserService userService;
 
     @Autowired
-    public AuthController(AuthService authService, TokenService tokenService) {
+    public AuthController(AuthService authService, TokenService tokenService, UserService userService) {
         this.authService = authService;
         this.tokenService = tokenService;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
@@ -69,6 +72,39 @@ public class AuthController {
         var token = generateUserToken(user.getEmail());
 
         return ResponseEntity.ok().body(Response.data(new TokenPayload(token)).build());
+    }
+
+    @PostMapping("/password/forgot")
+    public ResponseEntity<Response<PasswordPayload, AuthErrorCode>> forgotPassword(
+            @RequestBody ForgotPasswordInput input) {
+
+        Validation.validate(input);
+
+        var email = input.getEmail();
+        var user = userService.getByEmail(email);
+
+        if (user == null)
+            return ResponseEntity.ok().body(Response.data(new PasswordPayload(false)).build());
+
+        var token = generateUserToken(email);
+
+        authService.forgotPassword(email, token);
+
+        return ResponseEntity.ok().body(Response.data(new PasswordPayload(true)).build());
+    }
+
+    @PostMapping("/password/change")
+    public ResponseEntity<Response<PasswordPayload, AuthErrorCode>> changePassword(
+            @RequestBody ChangePasswordInput input) {
+
+        Validation.validate(input);
+
+        var context = tokenService.decode(input.getToken());
+        var email = context.get("email").asString();
+
+        authService.changePassword(email, input.getNewPassword());
+
+        return ResponseEntity.ok().body(Response.data(new PasswordPayload(true)).build());
     }
 
     private String generateUserToken(String email) {
