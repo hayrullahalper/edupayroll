c2c8dc2a1a04d7d97c52ec071c0015b97ee0e134
halis <isaycel2001@gmail.com>
2024-07-12 22:52:21 +0300

    refactor(web): update auth layout ui

--- packages/web/src/layouts/AuthLayout/AuthLayout.tsx
+++ packages/web/src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,21 +1,42 @@
-import { Flex } from '@mantine/core';
 import { Outlet } from 'react-router-dom';
+import { use100vh } from 'react-div-100vh';
+import { Flex, ScrollArea } from '@mantine/core';
+import { useLayoutEffect, useRef } from 'react';
 
 import Logo from '../../components/Logo';
 
 import styles from './AuthLayout.module.scss';
 
 export default function AuthLayout() {
+	const height = use100vh();
+	const viewportRef = useRef<HTMLDivElement>(null);
+
+	useLayoutEffect(() => {
+		if (!!viewportRef.current) {
+			const inner = viewportRef.current.querySelector('& > div');
+			const isScrollable =
+				viewportRef.current.scrollHeight > viewportRef.current.clientHeight;
+
+			if (!isScrollable) {
+				inner?.setAttribute('style', `height: ${height}px`);
+			}
+		}
+	}, [height]);
+
 	return (
-		<Flex
-			gap="xl"
-			align="center"
-			justify="center"
-			direction="column"
-			className={styles.container}
-		>
-			<Logo />
-			<Outlet />
-		</Flex>
+		<ScrollArea h={height || '100%'} viewportRef={viewportRef}>
+			<Flex
+				px="sm"
+				py="xl"
+				gap="xl"
+				align="center"
+				justify="center"
+				direction="column"
+				className={styles.container}
+			>
+				<Logo />
+				<Outlet />
+			</Flex>
+		</ScrollArea>
 	);
 }
