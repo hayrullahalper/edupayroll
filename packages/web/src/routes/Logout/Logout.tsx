6b1c4751a8d95dad47b6e3b50336df811f9b7fcd
halis <isaycel2001@gmail.com>
2024-07-12 23:13:17 +0300

    feat(web): create logout route component and utils

--- packages/web/src/routes/Logout/Logout.tsx
+++ packages/web/src/routes/Logout/Logout.tsx
@@ -1,5 +1,18 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { LoadingOverlay } from '@mantine/core';
 
+import paths from '../paths';
+import { useToken } from '../../contexts/token';
+
 export default function Logout() {
+	const navigate = useNavigate();
+	const { removeToken } = useToken();
+
+	useEffect(() => {
+		removeToken();
+		navigate(paths.home, { replace: true });
+	}, [navigate, removeToken]);
+
 	return <LoadingOverlay />;
 }
