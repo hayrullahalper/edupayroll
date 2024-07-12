7b8dedf2d74d49546ca3c2e4c971b672c6e1d7ef
halis <isaycel2001@gmail.com>
2024-07-12 23:11:43 +0300

    feat(web): add new providers

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,17 +1,31 @@
-import { BrowserRouter } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
+import { BrowserRouter } from 'react-router-dom';
+import { Notifications } from '@mantine/notifications';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import light from './themes/light';
 import Routes from './routes/Routes';
+import { UserProvider } from './contexts/user';
+import { TokenProvider } from './contexts/token';
 
 import './globals.scss';
 
+const queryClient = new QueryClient();
+
 export default function App() {
 	return (
-		<MantineProvider theme={light}>
-			<BrowserRouter>
-				<Routes />
-			</BrowserRouter>
-		</MantineProvider>
+		<QueryClientProvider client={queryClient}>
+			<MantineProvider theme={light}>
+				<TokenProvider>
+					<UserProvider>
+						<BrowserRouter>
+							<Routes />
+						</BrowserRouter>
+					</UserProvider>
+				</TokenProvider>
+
+				<Notifications position="top-right" />
+			</MantineProvider>
+		</QueryClientProvider>
 	);
 }
