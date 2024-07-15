2f03daa41e808a6331c2838a99e18e802f56aacf
Hayrullah <alperhayrullah@gmail.com>
2024-07-15 18:50:22 +0300

    fat(server): add exists by mail handler

--- packages/api/src/main/java/com/incubator/edupayroll/controller/user/UserExceptionHandler.java
+++ packages/api/src/main/java/com/incubator/edupayroll/controller/user/UserExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.incubator.edupayroll.controller.user;
 
+import com.incubator.edupayroll.service.user.UserExistsByEmailException;
 import com.incubator.edupayroll.service.user.UserNotFoundException;
 import com.incubator.edupayroll.service.user.UserPasswordMismatchException;
 import com.incubator.edupayroll.util.response.Response;
@@ -11,15 +12,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice(assignableTypes = UserController.class)
 public class UserExceptionHandler {
 
-  @ExceptionHandler({UserNotFoundException.class})
-  public ResponseEntity<Response<?, UserErrorCode>> handleInvalidCredentialsException() {
-    return ResponseEntity.status(HttpStatus.NOT_FOUND)
-        .body(Response.error(UserErrorCode.USER_NOT_FOUND, "User not found").build());
-  }
+    @ExceptionHandler({UserNotFoundException.class})
+    public ResponseEntity<Response<?, UserErrorCode>> handleInvalidCredentialsException() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Response.error(UserErrorCode.USER_NOT_FOUND, "User not found").build());
+    }
 
-  @ExceptionHandler({UserPasswordMismatchException.class})
+    @ExceptionHandler({UserPasswordMismatchException.class})
     public ResponseEntity<Response<?, UserErrorCode>> handleUserPasswordMismatchException() {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body(Response.error(UserErrorCode.PASSWORD_MISMATCH, "Password mismatch").build());
+                .body(Response.error(UserErrorCode.PASSWORD_MISMATCH, "Password mismatch").build());
     }
+
+    @ExceptionHandler({UserExistsByEmailException.class})
+    public ResponseEntity<Response<?, UserErrorCode>> handleUserExistsByEmailException() {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(Response.error(UserErrorCode.USER_EMAIL_EXISTS, "User exists by mail").build());
+    }
+
 }
