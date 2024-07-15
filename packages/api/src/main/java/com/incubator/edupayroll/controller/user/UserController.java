4e1ef03a956b63319f6645b221fc96516307b691
Hayrullah <alperhayrullah@gmail.com>
2024-07-15 18:42:50 +0300

    feat(server): add update methods

--- packages/api/src/main/java/com/incubator/edupayroll/controller/user/UserController.java
+++ packages/api/src/main/java/com/incubator/edupayroll/controller/user/UserController.java
@@ -1,8 +1,6 @@
 package com.incubator.edupayroll.controller.user;
 
-import com.incubator.edupayroll.dto.user.UserChangePasswordInput;
-import com.incubator.edupayroll.dto.user.User;
-import com.incubator.edupayroll.dto.user.UserChangePasswordPayload;
+import com.incubator.edupayroll.dto.user.*;
 import com.incubator.edupayroll.mapper.user.UserMapper;
 import com.incubator.edupayroll.service.user.UserService;
 import com.incubator.edupayroll.util.response.Response;
@@ -38,6 +36,27 @@ public class UserController {
         userService.changePassword(user, input.getCurrentPassword(), input.getNewPassword());
 
         return ResponseEntity.ok().body(Response.data(new UserChangePasswordPayload(true)).build());
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Response<UserUpdatePayload, UserErrorCode>> updateUser(@RequestBody UserNameUpdateInput input) {
+        Validation.validate(input);
+
+        var user = userService.getAuthenticatedUser();
+        userService.changeName(user, input.getFirstName(), input.getLastName());
+
+        return ResponseEntity.ok().body(Response.data(new UserUpdatePayload(true)).build());
+    }
+
+    @PutMapping("/update/email")
+    public ResponseEntity<Response<UserUpdatePayload, UserErrorCode>> updateUser(@RequestBody UserEmailUpdateInput input) {
+        Validation.validate(input);
+
+        var user = userService.getAuthenticatedUser();
+
+        userService.changeMail(user, input.getEmail(), input.getPassword());
+
+        return ResponseEntity.ok().body(Response.data(new UserUpdatePayload(true)).build());
     }
 
 }
