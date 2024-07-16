d69e01b697e3a0346b482a22a143181226af3d37
Hayrullah <alperhayrullah@gmail.com>
2024-07-16 11:26:26 +0300

    chore(server): apply code format

--- packages/api/src/main/java/com/incubator/edupayroll/dto/document/DocumentCreateInput.java
+++ packages/api/src/main/java/com/incubator/edupayroll/dto/document/DocumentCreateInput.java
@@ -3,27 +3,20 @@ package com.incubator.edupayroll.dto.document;
 import com.incubator.edupayroll.entity.export.ExportEntity;
 import com.incubator.edupayroll.entity.record.RecordEntity;
 import jakarta.validation.constraints.NotNull;
-import lombok.Value;
-
 import java.time.LocalDateTime;
 import java.util.List;
+import lombok.Value;
 
 @Value
 public class DocumentCreateInput {
 
-    @NotNull
-    public String name;
+  @NotNull public String name;
 
-    @NotNull
-    public LocalDateTime time;
+  @NotNull public LocalDateTime time;
 
-    @NotNull
-    public String description;
+  @NotNull public String description;
 
-    @NotNull
-    public List<ExportEntity> exports;
+  @NotNull public List<ExportEntity> exports;
 
-    @NotNull
-    public List<RecordEntity> records;
-
+  @NotNull public List<RecordEntity> records;
 }
