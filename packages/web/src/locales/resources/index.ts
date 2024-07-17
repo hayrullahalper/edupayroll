6c9fc4e0d25358dc1d2860da91e48bf89ef7d799
halis <isaycel2001@gmail.com>
2024-07-17 21:31:05 +0300

    feat(web): add new translations

--- packages/web/src/locales/resources/index.ts
+++ packages/web/src/locales/resources/index.ts
@@ -1,14 +1,18 @@
+import user from './tr/user.json';
 import auth from './tr/auth.json';
 import layout from './tr/layout.json';
 import common from './tr/common.json';
+import teachers from './tr/teachers.json';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
 	tr: {
 		translation: {
 			auth,
+			user,
 			common,
 			layout,
+			teachers,
 		} as const,
 	},
 } as const;
