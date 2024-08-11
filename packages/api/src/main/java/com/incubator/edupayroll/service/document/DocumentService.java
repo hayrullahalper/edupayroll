93cdc4261b2cbfe8d355d0d4c2c4a036e1b7bb9c
halis <isaycel2001@gmail.com>
2024-08-11 20:31:18 +0300

    feat(server): update document service and add missing methods

--- packages/api/src/main/java/com/incubator/edupayroll/service/document/DocumentService.java
+++ packages/api/src/main/java/com/incubator/edupayroll/service/document/DocumentService.java
@@ -1,23 +1,64 @@
 package com.incubator.edupayroll.service.document;
 
-import com.incubator.edupayroll.dto.document.DocumentCreateInput;
 import com.incubator.edupayroll.entity.document.DocumentEntity;
 import com.incubator.edupayroll.entity.user.UserEntity;
 import com.incubator.edupayroll.repository.DocumentRepository;
+import com.incubator.edupayroll.service.export.ExportService;
+import com.incubator.edupayroll.service.record.RecordService;
 import com.incubator.edupayroll.util.exception.AccessDeniedException;
 import java.time.YearMonth;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DocumentService {
-  final DocumentRepository documentRepository;
+  private final RecordService recordService;
+  private final ExportService exportService;
+  private final DocumentRepository documentRepository;
 
   @Autowired
-  public DocumentService(DocumentRepository documentRepository) {
+  public DocumentService(
+      RecordService recordService,
+      ExportService exportService,
+      DocumentRepository documentRepository) {
+    this.recordService = recordService;
+    this.exportService = exportService;
     this.documentRepository = documentRepository;
+  }
+
+  public long count(UserEntity user, Optional<String> name) {
+    return documentRepository.count(user, name.map(String::trim));
+  }
+
+  public List<DocumentEntity> getAll(
+      UserEntity user, int limit, int offset, Optional<String> name) {
+    int number = Math.round((float) offset / limit);
+
+    var sort = Sort.by(Sort.Direction.DESC, "createdAt").and(Sort.by(Sort.Direction.DESC, "id"));
+
+    var pr = PageRequest.of(number, limit, sort);
+    var page = documentRepository.findAllByUser(user, name.map(String::trim), pr);
+
+    return page.get().toList();
+  }
+
+  public DocumentEntity getById(UUID id) {
+    var maybeDocument = documentRepository.findById(id);
+    var document = maybeDocument.orElseThrow(() -> DocumentNotFoundException.byId(id));
+
+    var records = recordService.getAllByDocument(document);
+    var exports = exportService.getAllByDocument(document);
+
+    document.setRecords(records);
+    document.setExports(exports);
+
+    return document;
   }
 
   public DocumentEntity getById(UserEntity user, UUID id) {
@@ -27,6 +68,12 @@ public class DocumentService {
     if (!document.getUser().getId().equals(user.getId())) {
       throw AccessDeniedException.byUser(user);
     }
+
+    var records = recordService.getAllByDocument(document);
+    var exports = exportService.getAllByDocument(document);
+
+    document.setRecords(records);
+    document.setExports(exports);
 
     return document;
   }
@@ -43,18 +90,23 @@ public class DocumentService {
     return documentRepository.saveAndFlush(document);
   }
 
-  public DocumentEntity create(DocumentCreateInput input, UserEntity user) {
+  public DocumentEntity create(UserEntity user, String name, YearMonth time) {
     DocumentEntity document = new DocumentEntity();
 
     document.setUser(user);
 
-    document.setName(input.getName());
-    document.setTime(input.getTime());
+    document.setName(name);
+    document.setTime(time);
     document.setExports(new ArrayList<>());
     document.setRecords(new ArrayList<>());
 
-    return documentRepository.save(document);
+    return documentRepository.saveAndFlush(document);
   }
 
-  // TODO: implement remove and bulkRemove methods after implementation of exports and records
+  public void remove(DocumentEntity document) {
+    recordService.removeAllByDocument(document);
+    exportService.removeAllByDocument(document);
+
+    documentRepository.delete(document);
+  }
 }
