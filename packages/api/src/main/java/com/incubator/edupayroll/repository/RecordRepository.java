b8603e493cdbff76b12ece3e98490dfd244b6dfc
halis <isaycel2001@gmail.com>
2024-08-11 20:24:53 +0300

    feat(server): update record repository

--- packages/api/src/main/java/com/incubator/edupayroll/repository/RecordRepository.java
+++ packages/api/src/main/java/com/incubator/edupayroll/repository/RecordRepository.java
@@ -1,5 +1,6 @@
 package com.incubator.edupayroll.repository;
 
+import com.incubator.edupayroll.entity.document.DocumentEntity;
 import com.incubator.edupayroll.entity.record.RecordEntity;
 import java.util.Optional;
 import java.util.UUID;
@@ -8,5 +9,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecordRepository extends JpaRepository<RecordEntity, UUID> {
+  void deleteAllByDocument(DocumentEntity document);
+
   Optional<RecordEntity> findByHeadIsTrue();
+
+  Optional<RecordEntity> findByNext(RecordEntity next);
+
+  Optional<RecordEntity> findByHeadIsTrueAndDocument(DocumentEntity document);
 }
