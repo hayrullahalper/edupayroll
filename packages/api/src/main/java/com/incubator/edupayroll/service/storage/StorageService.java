1b6cd2a6a25c3f2fbf1466cf68241b5c7d929d35
halis <isaycel2001@gmail.com>
2024-09-24 01:00:53 +0300

    refactor(api): edit storage service and remove unnecessary related classes

--- packages/api/src/main/java/com/incubator/edupayroll/service/storage/StorageService.java
+++ packages/api/src/main/java/com/incubator/edupayroll/service/storage/StorageService.java
@@ -2,7 +2,9 @@ package com.incubator.edupayroll.service.storage;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import java.io.InputStream;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -19,38 +21,20 @@ public class StorageService {
     this.s3 = s3;
   }
 
-  public void uploadFile(String path, InputStream file) {
+  public void uploadFile(String path, InputStream stream) {
     try {
-      s3.putObject(bucketName, path, file, null);
+      var metadata = new ObjectMetadata();
+      metadata.setContentLength(stream.available());
+
+      s3.putObject(bucketName, path, stream, metadata);
     } catch (Exception e) {
       throw StorageUploadException.byException(e);
     }
   }
 
-  public InputStream downloadFile(String path) {
-    try {
-      return s3.getObject(bucketName, path).getObjectContent();
-    } catch (AmazonS3Exception e) {
-      throw StorageDownloadException.byException(e);
-    }
-  }
-
-  public StorageFileMetadata getFileMetadata(String path) {
-    try {
-      var data = s3.getObjectMetadata(bucketName, path);
-      var metadata = new StorageFileMetadata();
-
-      metadata.url = s3.getUrl(bucketName, path).toString();
-      metadata.path = path;
-      metadata.name = path.substring(path.lastIndexOf('/') + 1);
-      metadata.contentLength = data.getContentLength();
-      metadata.contentType = data.getContentType();
-      metadata.contentEncoding = data.getContentEncoding();
-
-      return metadata;
-    } catch (AmazonS3Exception e) {
-      throw StorageMetadataException.byException(e);
-    }
+  public String createSignedUrl(String path) {
+    var expiration = new Date(System.currentTimeMillis() + 2 * 60 * 60 * 1000);
+    return s3.generatePresignedUrl(bucketName, path, expiration).toString();
   }
 
   public void deleteFile(String path) {
