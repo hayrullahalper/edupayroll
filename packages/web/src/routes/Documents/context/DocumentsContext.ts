318eaa9bb98580f3404b273605e7685af577940b
halis <isaycel2001@gmail.com>
2024-09-29 12:58:09 +0300

    feat(web): remove unnecessary and missing documents context

--- packages/web/src/routes/Documents/context/DocumentsContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-export interface DocumentsContextProps {
-	create: () => void;
-	update: (id: string) => void;
-	remove: (ids: string[]) => void;
-}
-
-export default createContext<DocumentsContextProps | null>(null);
